2f60a0a2e3cae4df54ca392f9beee674657df892
dilek-technostudy <dileknuranyildirim@gmail.com>
2020-01-09 20:00:27 -0800

    ok

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,14 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+
+        @Test
+        public void test(){
+            boolean someCondition = true;
+            Assert.assertTrue( someCondition );
+        }
+    }
+
+
